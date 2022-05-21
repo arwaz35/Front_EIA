faf450e7321458d5cc99bde83a7ac73beaede795
daniel <daveduq@gmail.com>
2022-05-20 20:44:10 -0500

    11

--- pages/createmachine.js
+++ pages/createmachine.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import SideMenu from "../components/sideMenu";
+import SideMenu from "../components/SideMenu";
 import Header from "../components/Header";
 import React, { useState } from "react";
 

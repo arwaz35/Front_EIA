c1201c3cad6b24cb456312dd0e7b8ea064bdb946
daniel <daveduq@gmail.com>
2022-05-20 20:34:06 -0500

    Fith commit

--- pages/dashboard_OLD.js
+++ pages/dashboard_OLD.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import SideMenu from "../components/sideMenu";
+import SideMenu from "../components/SideMenu";
 import Header from "../components/Header";
 import Graph from "../components/Graph";
 import RTGraph from "../components/RTGraph";
